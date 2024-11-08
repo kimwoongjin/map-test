4dd495309f1205c0af757ca2383fda099ea51ebc
kimwoongjin <woong1536@gmail.com>
2024-11-08 15:24:33 +0900

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Map from './components/map/Map';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div id="root">
       <Navbar />
-      <Map />
+      <Routes>
+        <Route path="/" element={<Map />} />
+        <Route path="3d" element={<div>hello</div>} />
+      </Routes>
     </div>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom';
 import './navbar.css';
 
 export default function Navbar() {
   return (
     <div className="heading">
-      <h1>This is my map App</h1>
+      <h2>My App</h2>
+      <div className="nav">
+        <Link to={'/'} className="nav_item">
+          home
+        </Link>
+        <Link to={'/3d'} className="nav_item">
+          3D
+        </Link>
+      </div>
     </div>
   );
 }
